5dc96feb2e40c74f06989f36ee0167b2631414d7
Komal Sharma <komal.sharma@argusoft.com>
2026-08-24 19:15:38 +0530

    starting with express js server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log("Starting a new project");
+const express = require("express");
+const app = express();
+
+//Request Handlers
+app.use("/hello", (req, res) => {
+    res.send("Hello page");
+});
+
+app.use("/test", (req, res) => {
+    res.send("Test page");
+});
+
+app.use("/", (req, res) => {
+    res.send("Root page");
+});
+
+
+app.listen(3000, () => {
+    console.log("Server is successfully listening on port 3000");
+});
