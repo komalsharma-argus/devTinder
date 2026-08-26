9f4ad43fdefbc01f59312d6bf5d35c262bc8961e
Komal Sharma <komal.sharma@argusoft.com>
2026-08-26 14:47:38 +0530

    GET APIs

--- src/app.js
+++ src/app.js
@@ -24,6 +24,32 @@ app.post('/signup', async(req, res) => {
     }
 });
 
+//GET user by email
+app.get('/user', async(req, res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const users = await User.findOne({emailId: userEmail});
+        if(users.length === 0){
+            res.status(404).send("User Not Found!!");
+        }else{
+            res.send(users);
+        }
+    }catch(err){
+        res.status(404).send("Something went wrong!!");
+    }
+});
+
+//GET all users from the db
+app.get('/feed', async(req, res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(err){
+        res.status(404).send("SOmething went wrong!");
+    }
+})
+
 connectDB().then(() => {
     console.log("Database connected successfully!");
     app.listen(3000, () => {
