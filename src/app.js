8638b6763959846bbf272063db3e5e2952014b50
Komal Sharma <komal.sharma@argusoft.com>
2026-09-01 15:23:29 +0530

    API for fetching received connection requests

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const connectDB = require("./config/database");
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
+const userRouter = require("./routes/user");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,6 +19,7 @@ app.use(cookieParser());
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
+app.use("/", userRouter);
 
 connectDB().then(() => {
     console.log("Database connected successfully!");
